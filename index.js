0c42482b937275b383cb8c00d7a5387413eabaef
Keith Johnston <kjohnston@cbtnuggets.com>
2015-11-03 12:52:50 -0800

    Made rule path relative.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   'extends': [
-    'eslint-config-cbtnuggets/rules/node.js',
+    './rules/node.js',
 
     // See http://eslint.org/docs/rules/ for recommended rules
     'eslint:recommended'
