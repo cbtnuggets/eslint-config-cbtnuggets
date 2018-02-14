0c1152d2fa3eb2caf81caca9267cbd599794ac71
Derek Davis <ddavis@cbtnuggets.com>
2018-02-14 14:43:09 -0800

    Upgrade to eslint v4

--- rules/shared/react.js
+++ rules/shared/react.js
@@ -174,8 +174,10 @@ module.exports = {
     'react/self-closing-comp': 2,
 
     // Enforce spaces before the closing bracket of self-closing JSX elements
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-space-before-closing.md
-    'react/jsx-space-before-closing': [2, 'always'],
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-tag-spacing.md
+    'react/jsx-tag-spacing': [2, {
+        beforeSelfClosing: 'always'
+    }],
 
     // Enforce component methods order
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-comp.md
